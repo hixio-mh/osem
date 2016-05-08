0d272b38273be138603d6c3e8a649aa2d0f94977
shane tully <shane@shanetully.com>
2016-05-07 21:40:06 -0700

    adapt deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,23 @@
 require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
+require 'mina/rvm'
 
-set :domain, 'proxy-opensuse.suse.de'
-set :port, 2214
-set :user, 'osem'
-set :deploy_to, '/srv/www/vhosts/opensuse.org/events'
-set :repository, 'https://github.com/openSUSE/osem.git'
+set :domain, 'osem.seagl.org'
+set :user, 'ubuntu'
+set :deploy_to, '/srv/www/osem'
+set :repository, 'https://github.com/seagl/osem.git'
+set :term_mode, nil
+set :rvm_path, '/usr/local/rvm/bin/rvm'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, %w{ config/database.yml log public/system config/secrets.yml config/piwik.yml tmp}
+set :shared_paths, %w{ log public/system tmp}
+
+task :environment do
+  # For those using RVM, use this to load an RVM version@gemset.
+  invoke :'rvm:use[ruby-2.3.0]'
+end
 
 task setup: :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/log"]
@@ -19,11 +26,8 @@ task setup: :environment do
   queue! %[mkdir -p "#{deploy_to}/shared/config"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/config"]
 
-  queue! %[touch "#{deploy_to}/shared/config/database.yml"]
-
   queue! %[touch "#{deploy_to}/shared/system"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/shared/system"]
-  queue  %[echo "-----> Be sure to edit 'shared/config/database.yml'."]
 end
 
 desc 'Deploys the current version to the server.'
@@ -39,7 +43,6 @@ task deploy: :environment do
     invoke :'dump_db'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
-    #invoke :notify_errbit
 
     to :launch do
       queue "sudo /etc/init.d/apache2 restart"
@@ -48,13 +51,6 @@ task deploy: :environment do
 
     invoke :'deploy:cleanup'
   end
-end
-
-desc 'Notifies the exception handler of the deploy.'
-task :notify_errbit do
-  revision = `git rev-parse HEAD`.strip
-  user = ENV['USER']
-  queue "cd #{deploy_to}/current && RAILS_ENV=production bundle exec rake hoptoad:deploy TO=#{rails_env} REVISION=#{revision} REPO=#{repository} USER=#{user}"
 end
 
 desc 'Back ups the database'
