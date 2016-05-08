ea439e9aab1e3bc7789195ca579520ae7bb77641
shane tully <shane@shanetully.com>
2016-05-07 17:13:41 -0700

    add config files from server to repo

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Load the app's custom environment variables here, so that they are loaded before environments/*.rb
+env_vars = File.join(Rails.root, 'config', 'env_vars.rb')
+load(env_vars) if File.exist? env_vars
+
 # Load the configuration file
 path = Rails.root.join('config', 'config.yml')
 if File.exist?(path)
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,15 +72,15 @@ Osem::Application.configure do
   # Set the smtp configuration of your service provider
   # For further details of each configuration checkout: http://guides.rubyonrails.org/action_mailer_basics.html#action-mailer-configuration
   config.action_mailer.smtp_settings = {
-    address:              ENV['OSEM_SMTP_ADDRESS'],
-    port:                 ENV['OSEM_SMTP_PORT'],
-    user_name:            ENV['OSEM_SMTP_USERNAME'],
-    password:             ENV['OSEM_SMTP_PASSWORD'],
-    authentication:       ENV['OSEM_SMTP_AUTHENTICATION'].try(:to_sym),
-    domain:		  ENV['OSEM_SMTP_DOMAIN'],
-    enable_starttls_auto: true
+    address: ENV['OSEM_SMTP_ADDRESS'],
+    port: ENV['OSEM_SMTP_PORT'],
+    user_name:ENV['OSEM_SMTP_USERNAME'],
+    password: ENV['OSEM_SMTP_PASSWORD'],
+    authentication: ENV['OSEM_SMTP_AUTHENTICATION'].try(:to_sym),
+    domain: ENV['OSEM_SMTP_DOMAIN'],
+    enable_starttls_auto: true,
   }
 
   # Set the secret_key_base from the env, if not set by any other means
-  config.secret_key_base ||= ENV["SECRET_KEY_BASE"]
+  config.secret_key_base ||= ENV['SECRET_KEY_BASE']
 end
