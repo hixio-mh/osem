956221067dbcb220bd89308c77d6980f8f4f5b62
shane tully <shane@shanetully.com>
2016-05-08 13:49:12 -0700

    add banner image field to splashpages

--- spec/factories/splashpages.rb
+++ spec/factories/splashpages.rb
@@ -16,6 +16,14 @@ FactoryGirl.define do
       include_sponsors true
       include_lodgings true
       include_cfp true
+
+      after :create do |splashpage|
+        # Banner...
+        uploader = PictureUploader.new(splashpage, :picture)
+        File.open('app/assets/images/rails.png') { |f| uploader.store!(f) }
+        splashpage.banner_file_name = 'rails.png'
+        splashpage.save
+      end
     end
   end
 end
